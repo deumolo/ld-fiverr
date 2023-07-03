ef84b946d3fc5a2c767e9e1418fc7afe83a312e0
Daniel <deumolo@gmail.com>
2023-07-02 20:36:02 -0600

    Added slider to homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import	"./Home.scss"
+import React from 'react';
+import './Home.scss';
+import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/trustedBy/TrustedBy';
+import Slide from '../../components/slide/Slide';
+import { cards } from '../../data';
+import Slider from 'infinite-react-carousel';
+import CatCard from '../../components/catCard/CatCard';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>
+        <Featured />
+        <TrustedBy />
+        <Slide children={cards} slidesToShow={5} arrowsScroll={4}>
+          {cards.map((card) => {
+            return (
+              <div key={card.id}>
+                <CatCard card={card} />
+              </div>
+            );
+          })}
+        </Slide>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
