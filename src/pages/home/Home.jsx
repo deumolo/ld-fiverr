00ee41c28c1bfe6e0206d4c6f7c75aef55532a53
Daniel <deumolo@gmail.com>
2023-07-03 12:09:01 -0600

    Added featured content in homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ import CatCard from '../../components/catCard/CatCard';
 const Home = () => {
   return (
     <>
-      <div>
+      <div className="home">
         <Featured />
         <TrustedBy />
         <Slide children={cards} slidesToShow={5} arrowsScroll={4}>
@@ -22,6 +22,80 @@ const Home = () => {
             );
           })}
         </Slide>
+        <div className="features">
+          <div className="container">
+            <div className="item">
+              <h1>A whole world of freelance talent at your fingertips</h1>
+              <div className="title">
+                <img src="./img/check.png" alt="" />
+                The best of every budget
+              </div>
+              <p>
+                Find the right service for every price point. No hourly rates,
+                just project-based pricing.
+              </p>
+              <div className="title">
+                <img src="./img/check.png" alt="" />
+                The best of every budget
+              </div>
+              <p>
+                Find the right service for every price point. No hourly rates,
+                just project-based pricing.
+              </p>
+              <div className="title">
+                <img src="./img/check.png" alt="" />
+                The best of every budget
+              </div>
+              <p>
+                Find the right service for every price point. No hourly rates,
+                just project-based pricing.
+              </p>
+              <div className="title">
+                <img src="./img/check.png" alt="" />
+                The best of every budget
+              </div>
+              <p>
+                Find the right service for every price point. No hourly rates,
+                just project-based pricing.
+              </p>
+            </div>
+            <div className="item">
+              <video src="./img/video.mp4" controls></video>
+            </div>
+          </div>
+        </div>
+        <div className="features dark">
+          <div className="container">
+            <div className="item">
+              <h1>fiverr business</h1>
+              <h1>A solution built for business</h1>
+              <p>
+                Upgrade to a curated experience to access vetted talent and
+                exclusive tools
+              </p>
+              <div className="title">
+                <img src="./img/check.png" alt="" />
+                Talent matching
+              </div>
+              <div className="title">
+                <img src="./img/check.png" alt="" />
+                Dedicated account management
+              </div>
+              <div className="title">
+                <img src="./img/check.png" alt="" />
+                Team collaboration tools
+              </div>
+              <div className="title">
+                <img src="./img/check.png" alt="" />
+                Business payment solutions
+              </div>
+              <button>Explore Fiverr Business </button>
+            </div>
+            <div className="item">
+              <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
