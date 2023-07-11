c33196e7c809c0565700b5f843c03049580ccae6
Daniel <deumolo@gmail.com>
2023-07-11 15:06:39 -0600

    Removed unused import in homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,6 @@ import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
 import Slide from '../../components/slide/Slide';
 import { cards, projects } from '../../data';
-import Slider from 'infinite-react-carousel';
 import CatCard from '../../components/catCard/CatCard';
 import ProjectCard from '../../components/projectCard/ProjectCard';
 
