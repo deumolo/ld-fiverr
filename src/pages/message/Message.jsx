00d2b32c6f570d7c22cbb76a06b5eec6d0c97c88
Daniel <deumolo@gmail.com>
2023-07-13 10:58:10 -0600

    Added static content and css rules to message page

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import "./Message.scss"
+import { Link } from 'react-router-dom'
+
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message'>
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link className='link' to="/messages">
+            MESSAGES
+          </Link>
+          &gt;
+          John Doe
+          &gt;
+        </span>
+
+        <div className="messages">
+          <div className="item">
+            <img className='pp' src="https://placekitten.com/408/287" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ducimus quisquam placeat corrupti sequi eos quos magnam cupiditate sapiente saepe quibusdam illo illum nam facilis obcaecati, eum optio! Minima, animi?
+            </p>
+          </div>
+          <div className="item owner">
+            <img className='pp' src="https://placekitten.com/408/287" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ducimus quisquam placeat corrupti sequi eos quos magnam cupiditate sapiente saepe quibusdam illo illum nam facilis obcaecati, eum optio! Minima, animi?
+            </p>
+          </div>
+          <div className="item">
+            <img className='pp' src="https://placekitten.com/408/287" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ducimus quisquam placeat corrupti sequi eos quos magnam cupiditate sapiente saepe quibusdam illo illum nam facilis obcaecati, eum optio! Minima, animi?
+            </p>
+          </div>
+          <div className="item owner">
+            <img className='pp' src="https://placekitten.com/408/287" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ducimus quisquam placeat corrupti sequi eos quos magnam cupiditate sapiente saepe quibusdam illo illum nam facilis obcaecati, eum optio! Minima, animi?
+            </p>
+          </div>
+          <div className="item">
+            <img className='pp' src="https://placekitten.com/408/287" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ducimus quisquam placeat corrupti sequi eos quos magnam cupiditate sapiente saepe quibusdam illo illum nam facilis obcaecati, eum optio! Minima, animi?
+            </p>
+          </div>
+          <div className="item owner">
+            <img className='pp' src="https://placekitten.com/408/287" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ducimus quisquam placeat corrupti sequi eos quos magnam cupiditate sapiente saepe quibusdam illo illum nam facilis obcaecati, eum optio! Minima, animi?
+            </p>
+          </div>
+        </div>
+
+        <hr />
+        
+        <div className="write">
+          <textarea name="" placeholder='write a message' id="" cols="30" rows="10"></textarea>
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
