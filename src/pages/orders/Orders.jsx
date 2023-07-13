b63d110abf973b4800760f20932ca5fe83fa7c8d
Daniel <deumolo@gmail.com>
2023-07-13 10:10:45 -0600

    Added static content and css rules to orders page

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import "./Orders.scss"
+import { Link } from 'react-router-dom';
 
 const Orders = () => {
+
+  const currentUser = {
+    id: 1,
+    username: 'John Kramer',
+    isSeller: true,
+  };
+
   return (
-    <div>Orders</div>
+    <div className='orders'>
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{ currentUser.isSeller ? 'Buyer' : 'Seller' }</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td><img className='img' src="https://placekitten.com/408/287" alt="" /></td>
+            <td>Gig 1</td>
+            <td>$99.99</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="./img/message.png" alt="" />
+
+            </td>
+          </tr> 
+          <tr>
+            <td><img className='img' src="https://placekitten.com/408/287" alt="" /></td>
+            <td>Gig 1</td>
+            <td>$99.99</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="./img/message.png" alt="" />
+
+            </td>
+          </tr> 
+          <tr>
+            <td><img className='img' src="https://placekitten.com/408/287" alt="" /></td>
+            <td>Gig 1</td>
+            <td>$99.99</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="./img/message.png" alt="" />
+
+            </td>
+          </tr> 
+        </table>
+      </div>
+    </div>
   )
 }
 
